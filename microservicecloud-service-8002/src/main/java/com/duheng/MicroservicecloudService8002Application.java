5b0c6bcaf1138d2719417653d5ddc09f92b708ed
DuHeng <DuHeng@sina.com>
2020-02-21 00:08:47 +0800

    Eureka的服务端集群搭建

--- microservicecloud-service-8002/src/main/java/com/duheng/MicroservicecloudService8002Application.java
+++ microservicecloud-service-8002/src/main/java/com/duheng/MicroservicecloudService8002Application.java
@@ -1,17 +1,15 @@
 package com.duheng;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@MapperScan("com.duheng.mapper")//扫描Mapper接口
 @EnableEurekaClient
-public class MicroservicecloudService8001Application {
+public class MicroservicecloudService8002Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(MicroservicecloudService8001Application.class, args);
+        SpringApplication.run(MicroservicecloudService8002Application.class, args);
     }
 
 }
---
+++ microservicecloud-service-8002/src/test/java/com/duheng/MicroservicecloudService8002ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duheng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicecloudService8002ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
